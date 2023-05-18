34bba10502e81fa12f21279a4fd099c523d16bd1
JobaerAbdi <jobaerrahman757@gmail.com>
2023-05-18 21:17:58 +0600

    Gallery section done

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,56 @@
+const Gallery = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:px-14">
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDOH-BABY-BABYOH-B18538490F8E7BA/1564603894575_0..jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+        <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDEASY-MART-OMLION660908FFFCFC81/1588600179222_5.jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+        <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDOH-BABY-BABYOH-B18538443CB7C17/1615539609878_11.jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDJEEP-801-BATLION66090831D0486/1588576781835_4.jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDOH-BABY-BABYOH-B18538434ACEF66/1615539607705_13.jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+        <figure>
+        <img
+            src="https://assetscdn1.paytm.com/images/catalog/product/K/KI/KIDEASY-MART-OMLION6609087CAEC03E/1588600191175_11.jpg?imwidth=320&impolicy=hq"
+            alt="car"
+          />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
