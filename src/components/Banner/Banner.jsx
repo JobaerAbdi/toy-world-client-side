eb7fe86977fb7bdfb3e8bcd4109926a76abada79
JobaerAbdi <jobaerrahman757@gmail.com>
2023-05-18 20:20:12 +0600

    Banner section complete

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,43 @@
+const Banner = () => {
+  return (
+    <div className="flex flex-col lg:flex-row lg:px-16 mt-5 mb-10">
+      <div className="sm:w-full mb-5 lg:w-3/5">
+        <img
+          className="w-full h-screen"
+          src="https://secure.img1-cg.wfcdn.com/im/68928481/resize-h800-w800%5Ecompr-r85/1703/170365550/Kids+12v+Electric+Ride+On+Lamborghini+Toy+Car+Vehicles+With+Remote+Control%2C+Battery+Powered%2C+Led+Lights%2C+Wheels+Suspension%2C+Music%2C+Horn%2C+Compatible+With+Lamborghini%2C+Red.jpg"
+          alt=""
+        />
+      </div>
+      <div className="sm:w-full lg:w-2/5 ms-7">
+        <p className="font-semibold">
+          Growing up, there were few things I enjoyed more than my ride-on
+          electric car. Mine was a four-wheeler-looking thing, similar to my
+          number 1 below, that I used to traverse the small hill behind the
+          house I grew up in. It had a plastic seat and wheels, and while no one
+          would ever confuse it as an off-road vehicle, I honestly felt like I
+          could conquer the world driving it around. These electric toys have
+          gotten even more remarkable over the years. No longer is it just a
+          generic 4-wheeler or generic jeep-looking toy; there are actual brands
+          and models available. They’ve also improved safety by adding
+          seatbelts, lower top speeds, and insulated battery areas. I may not
+          remember every time I tipped over my old toy 4-wheeler, but I am sure
+          my parents do. Growing up, I may not have had access to the latest and
+          greatest electric toy cars, but kids nowadays do. So here is a
+          shortlist of some of the more exciting toy cars available for your
+          hill conquering kids at home. These electric toys have
+          gotten even more remarkable over the years. No longer is it just a
+          generic 4-wheeler or generic jeep-looking toy; there are actual brands
+          and models available. They’ve also improved safety by adding
+          seatbelts, lower top speeds, and insulated battery areas. These electric toys have
+          gotten even more remarkable over the years. No longer is it just a
+          generic 4-wheeler or generic jeep-looking toy; there are actual brands
+          and models available. They’ve also improved safety by adding
+          seatbelts, lower top speeds, and insulated battery areas.
+          Rough, rugged, and fun to drive–the Power Wheels Ford F-150 Raptor Extreme is just like the real thing. It’s designed with oversized tires and a lifted body so that young drivers can power over the roughest backyard driving conditions, including wet grass, mud, and more. There’s a roomy truck bed with a working lift-and-lower tailgate, storage inside the dash, and a pretend radio with pre-loaded tunes, sound effects, and a voice-amplifying microphone. The Power Wheels F-150 Raptor seats two, drives two speeds forward (2.5 or 5 mph max) plus reverse, and is equipped with Power Lock brakes and a parent-controlled high-speed lockout for beginner drivers.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
